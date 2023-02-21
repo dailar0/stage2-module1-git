f8ad2b3410ab9696a833935f0b39156b00036d4e
dailar0 <90409024+dailar0@users.noreply.github.com>
2023-02-21 07:37:08 +0300

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/dailar0/stage2-module1-git-task1";
+    public static String REPO_LINK = "https://github.com/dailar0/stage2-module1-git-task2";
 }
